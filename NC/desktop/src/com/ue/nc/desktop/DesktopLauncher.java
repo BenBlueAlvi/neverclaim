f2f46cc7b6a53ebb83a1a37714b72881487a4a6f
EpicBlueCircles <benbluealvi@gmail.com>
2018-11-28 19:56:23 -0800

    Initial commit

---
+++ NC/desktop/src/com/ue/nc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ue.nc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ue.nc.NCgame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NCgame(), config);
+	}
+}
